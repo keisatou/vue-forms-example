93be85ba38626a45991553d84a43687783979adc
keisatou <dummy>
2017-04-16 19:24:32 +0900

    change parent status from child component
    this makes use of how v-model works behind the scenes.
    see comment below:
    14                    :value="userData.email"
    15                    @input="userData.email = $event.target.value">
    16                    <!-- above is how v-model works behind the scenes! -->

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <app-switch></app-switch>
+          <app-switch v-model="dataSwitch"></app-switch>
         </div>
       </div>
       <hr>
@@ -127,7 +127,7 @@
             </ul>
             <p>Gender: {{ gender }}</p>
             <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched:</p>
+            <p>Switched: {{ dataSwitch }}</p>
           </div>
         </div>
       </div>
@@ -151,6 +151,7 @@ export default {
       gender: 'Male',
       selectedPriority: 'High',
       priorities: ['High', 'Medium', 'Low'],
+      dataSwitch: true,
     };
   },
   components: {
